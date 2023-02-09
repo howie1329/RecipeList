ce91ba4d41b5034d82090a5e267de7406544cbc0
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-02-09 05:24:22 -0500

    Initial Commit

---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Howard Thomas on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
